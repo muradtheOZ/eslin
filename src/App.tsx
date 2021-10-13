7568c14cc2e7c34cab380917967b64a008769f75
Murad Ahmed <muradahmed199771@gmail.com>
2021-10-13 20:24:14 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from './components/common/Header';
+import AppRouter from './services/AppRouter';
+
+function App() {
+  return (
+    // var bro = "hjh";
+    <>
+      <AppRouter>
+        <Header />
+      </AppRouter>
+    </>
+  );
+}
+
+export default App;
